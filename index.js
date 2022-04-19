15b3671372806c5c60900fa55d69a0ba89f9876e
RamonoesQ <ramonqueirozs@gmail.com>
2022-04-18 21:54:18 -0300

    nova rota
    configurada a post para cadastro de cursos

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const routes = require("./routes/index")
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
-app.use(routes);
+app.use(routes); 
 
 app.listen(4000,()=> console.log("Servidor rodando na porta 4000"));
